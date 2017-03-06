45d586a48582b252e9755db750d4326a48e61e7e
Joshua Chang <j2chang@ucsd.edu>
2017-03-06 12:31:31 -0800

    Added config.js for Firebase

---
+++ public/config.js
@@ -0,0 +1,13 @@
+/*
+ * config.js: contains Firebase configuration information and initialization
+ */
+
+
+var config = {
+apiKey: "AIzaSyCJVqf5xwVrKMIwOnl216spZI3FVoZBx0g",
+authDomain: "marveldex-c8644.firebaseapp.com",
+databaseURL: "https://marveldex-c8644.firebaseio.com",
+storageBucket: "marveldex-c8644.appspot.com",
+messagingSenderId: "422461575050"
+};
+firebase.initializeApp(config);
